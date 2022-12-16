3fc6b2b0f4ce6923348d8e4d0b8a1f5415c39110
Kelvin <ngotho.kelvin@yahoo.com>
2022-12-16 13:40:51 +0300

    comfigure webpack &  create to do list structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-// import _ from 'lodash';
+import "bootstrap/dist/css/bootstrap.css";
+import displayToDos from "./todolist/script.js";
 import "./style.css";
 
-const task = [
-  { description: "Cook lunch", isCompleted: false, index: 0 },
-  { description: "Do laundry", isCompleted: false, index: 1 },
-  { description: "Cook dinner", isCompleted: false, index: 2 },
-];
-
-const taskList = () => {
-  let taskListContent = "";
-  task.forEach((item) => {
-    taskListContent += `<li class="activity"><input class="activity-check" type="checkbox"><span class="list">${item.description}</span></li>`;
-  });
-  document.querySelector(".activity-container").innerHTML = taskListContent;
-};
-taskList();
+displayToDos();
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,34 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main[contentHash].js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          // "sass-loader"
+        ],
       },
     ],
   },
-  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: "auto",
+    hot: true,
+  },
 };
