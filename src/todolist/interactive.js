31cabba8d1cde035a1e921e0e5899b2fff48fd2b
Kelvin <ngotho.kelvin@yahoo.com>
2022-12-20 16:13:35 +0300

    use a switch statement

--- src/todolist/interactive.js
+++ src/todolist/interactive.js
@@ -1,44 +1,38 @@
-export default class Interactive {
-  constructor() {
-    this.toDoList = JSON.parse(localStorage.getItem('todo')) || [];
-  }
+export default function Interactive() {
+  const toDoList = JSON.parse(localStorage.getItem('todo')) || [];
 
-  // update status
-  updateStatus(checkbox, index) {
+  const updateStatus = (checkbox, index) => {
     checkbox.addEventListener('click', (event) => {
-      if (event.currentTarget.checked) {
-        checkbox.nextElementSibling.classList.add('text-decoration-line-through');
-        this.toDoList[index].completed = true;
-        localStorage.setItem('todo', JSON.stringify(this.toDoList));
-      } else {
-        if (checkbox.nextElementSibling.classList.contains('text-decoration-line-through')) {
-          checkbox.nextElementSibling.classList.remove('text-decoration-line-through');
-        }
-        this.toDoList[index].completed = false;
-        localStorage.setItem('todo', JSON.stringify(this.toDoList));
-      }
+      const actionMap = {
+        'click': (target) => target.nextElementSibling.classList.add('text-decoration-line-through'),
+        'unclick': (target) => target.nextElementSibling.classList.remove('text-decoration-line-through'),
+      };
+
+      const action = event.currentTarget.checked ? 'click' : 'unclick';
+      const target = event.currentTarget;
+      actionMap[action](target);
+      toDoList[index].completed = event.currentTarget.checked;
+      localStorage.setItem('todo', JSON.stringify(toDoList));
     });
   }
 
-  // remove all completed tasks
-  removeCompleted(removeCompleted) {
+  const removeCompleted = (removeCompleted) => {
     removeCompleted.addEventListener('click', () => {
-      this.toDoList = this.toDoList.filter((task) => task.completed !== true);
-      localStorage.setItem('todo', JSON.stringify(this.toDoList));
-      this.toDoList.forEach((task, index) => {
+      toDoList = toDoList.filter((task) => task.completed !== true);
+      localStorage.setItem('todo', JSON.stringify(toDoList));
+      toDoList.forEach((task, index) => {
         if (task.index !== index) {
           task.index = index;
         }
       });
-      localStorage.setItem('todo', JSON.stringify(this.toDoList));
+      localStorage.setItem('todo', JSON.stringify(toDoList));
       window.location.reload();
     });
   }
-
-  // add event listener on reload icon
-  static reload(refresh) {
-    refresh.addEventListener('click', () => {
-      window.location.reload();
-    });
-  }
+ // add event listener on reload icon
+ static reload(refresh) {
+  refresh.addEventListener('click', () => {
+    window.location.reload();
+  });
+}
 }
